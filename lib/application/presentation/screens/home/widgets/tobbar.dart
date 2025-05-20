21ce056df6f9fe0fda0cf793c0b3da589d4abaff
dibindasky2000@gmail.com <dibindasky2000@gmail.com>
2025-05-20 12:25:01 +0530

    folder added for controllers

--- lib/application/presentation/screens/home/widgets/tobbar.dart
+++ lib/application/presentation/screens/home/widgets/tobbar.dart
@@ -1,4 +1,4 @@
-import 'package:agent_dashboard/application/controller/home.dart';
+import 'package:agent_dashboard/application/controller/home/home.dart';
 import 'package:agent_dashboard/application/presentation/screens/home/widgets/logo.dart';
 import 'package:agent_dashboard/application/presentation/utils/colors.dart';
 import 'package:agent_dashboard/application/presentation/utils/constants.dart';
